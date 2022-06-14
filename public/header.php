0c2a04807eb77c475744ba0492dbd9dad9b64c02
EVELINA <evelinaghazzaoui@hotmail.com>
2022-06-14 11:27:22 +0200

    changes

---
+++ public/header.php
@@ -0,0 +1,21 @@
+<?
+require('../src/dbconnect.php');
+?>
+<link rel="stylesheet" href="css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<nav>
+    <div class ="header-column">
+      <div class="header">
+        <a href="mypage.php">My page</a>
+        <!-- <a href="#"><i class="fa-light fa-cart-shopping" style="font-size:24px">&#xf07a;</i></a> -->
+        <a href="#">Log out</a>
+        <a href="#"><i class="fa" style="font-size:22px">&#xf07a;</i></a>
+        <a href="#">🔍 </a>
+      </div>
+        <!-- <a href="#"> <h1 id="kiano">K I A N O</h1></a> -->
+        <div class="logo-img" >
+        <a href="#"> <img src="img/LOGOwithoutcircle.png" alt="" width ="350px" height="200px"></a>
+</div>
+</div>
+    </nav>
