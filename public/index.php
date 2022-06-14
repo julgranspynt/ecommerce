67b12f6fd4921a221585d8a3eead080cf8956af2
Emma Larsson <emmaaalarsson@hotmail.com>
2022-06-14 19:29:48 +0200

    Index gallery and header

--- public/index.php
+++ public/index.php
@@ -12,71 +12,102 @@
 	<?php include('./layout/header.php'); ?>
 
 	<main>
-		
 
+	
+		<div class="banner"><b>Special offer:</b> Order before sunday to get free delivery on all orders over 300 SEK.</div>
 		<div class="hero-container">
-		<img src="img/Hero.png" alt="Fruits and smootie" width="1500px">
-			<!-- <img src="https://cdn.mos.cms.futurecdn.net/vsEypm3JWGtFyX4wT228n-1200-80.jpg"> -->
+		<img src="img/Hero.png" alt="Fruits and smootie" width="1483px">
 			<div class="hero-text">
-				<!-- <h1>Fruits & Veggies</h1>
-				<p>- the easy way everyday</p> -->
+
 			</div>
 		</div>
 
 
-		<div class="some-products" style="text-align: center; height: 400px; border:1px solid rgba(61, 51, 51, 0.8);">
-			<h3>organic powder mixes of fruits & veggies - mix into water, plant-milk or a smoothie</h3>
-			<div class="" style="height: 300px;">
-				product row!
+		<div class="some-products" style="text-align: center; height: 450px; background-color:beige">
+			<h3>Organic powder mixes of fruits & veggies - mix into water, plant-milk or a smoothie</h3>
+			<div class="product-div" style="height: 300px;">
+
+			<div class="product-column">Product 1</div>
+			<div class="product-column">Product 2</div>
+			<div class="product-column">Product 3</div>
+			<div class="product-column">Product 4</div>
+				
 			</div>
-			<a href="products.php" style="	text-decoration: none;
-											background-color: pink;
-											color: #333333;
-											padding: 5px 10px 5px 10px;
-											border: 0.2px solid #333333;
-											border-radius: 3px;">View all our products</a>
+			
+			<button class="button" href="products.php">View all our products</button>
 		</div>
 
+<div id="multi-item-example" class="carousel slide carousel-multi-item carousel-multi-item-2" data-ride="carousel">
 
-		<div class="why-kiano" style="margin: 20px 0;
-    padding: 35px 0px 0px 55px;">
-			<div="left-column" style="	display: flex;
-    									justify-content: center;
-    									align-items: center;
-										width: 50%;
-										float: left;
-										margin-right: 5%;">
-				<img src="https://cdn.shopify.com/s/files/1/0550/6180/7318/files/startpage_69649080-a6e6-4ba3-ae6a-374a39596d00_720x.jpg">
-			</div>
-			<div="right-column" style="background-color: pink; width: 37%; float: left;">
-				<h1>why buy kiano</h1>
-				<p>We produce clean superfood powders that are organic, tasty - and free from artificial vitamins and minerals, emulsifiers and fillers. We include adaptogens in our powders that work as herbal pharmaceuticals, helping to calm the nervous system. Our superfood powders are densely packed with the micronutrients your body craves. And they're convenient and easy to use!</p>
-			</div>
-		</div>
-		
+  <div class="carousel-inner" role="listbox">
 
-		<div class="kiano-believes">
-			<h3>KIANO BELIEVES</h3>
-			<div class="belief">
-				<img class="qwe" src="https://cdn.shopify.com/s/files/1/0550/6180/7318/files/bb_360x.png?v=1633332682">
-				<h4>100% VEGAN AND ORGANIC</h4>
-				<p>We believe that a clean, vegan, nutritious diet is great for our general health - and the environment. Our powders have been crafted with only the finest organic ingredients and are rich in natural vitamins and minerals. All our mixes taste delicious and have numerous benefits for aiding and strengthening our physical and mental health.</p>
-			</div>
-			<div class="belief">
-				<img class="qwe" src="https://cdn.shopify.com/s/files/1/0550/6180/7318/files/Spicy_calm_photo_100_resolution_360x.jpg?v=1633332576">
-				<h4>PREMIUM INGREDIENTS</h4>
-				<p>Our range of organic vegan powders contain more than 45 different premium plant ingredients sourced from the highest quality farms around the world. We're proud of our production and stoked to be able to offer you 100% plant-based, gluten-free powders containing only natural sugars from nature.</p>
-			</div>
-			<div class="belief">
-				<img class="qwe" src="https://cdn.shopify.com/s/files/1/0550/6180/7318/files/profile-pic_540x.jpg?v=1633332531">
-				<h4>WE COLLABORATE WITH</h4>
-				<p>Naturopathic Doctor, Cora Forsten, ND Stockholm, Sweden: "KIANO’s organic, vegan line of superfood mixes are a great and accessible way to supercharge your diet and provide your body with many of the elements that it needs to optimise your health & meet your individual needs and goals!"</p>
-			</div>
-		</div>
+    <div class="carousel-item active">
 
-	</main>
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie4.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie7.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie6.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie5.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+
+    </div>
+
+    <div class="carousel-item">
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie1.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie2.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie3.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/smootie8.png"
+            alt="Card image cap" height="350px" width="auto">
+        </div>
+      </div>
+</div>
+
 
 	<?php include('./layout/footer.php'); ?>
 
+
 </body>
 </html>
+
