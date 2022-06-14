99e7db5ac644fe8ff8d1df73a20ac259d1f4729b
EVELINA <evelinaghazzaoui@hotmail.com>
2022-06-14 23:50:28 +0200

    header utloggad

---
+++ public/layout/header2.php
@@ -0,0 +1,18 @@
+<?
+require('../../src/dbconnect.php');
+?>
+<link rel="stylesheet" href="../css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<nav>
+    <div class ="header-column">
+      <div class="header">
+        <a href="./login.php">Logga in</a>
+        <a href="#"><i class="fa" style="font-size:22px">&#xf07a;</i></a>
+        <a href="#">🔍 </a>
+</div>
+        <div class="logo-img" >
+        <a href="./index.php"> <img src="../img/LOGOwithoutcircle.png" alt="" width ="350px" height="200px"></a>
+</div>
+</div>
+    </nav>
