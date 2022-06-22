443b4999b19a6ec6d981b1609f51f32091cb1d0e
EVELINA <evelinaghazzaoui@hotmail.com>
2022-06-22 09:55:54 +0200

    ok

--- public/layout/header.php
+++ public/layout/header.php
@@ -24,7 +24,7 @@ if(isset($_POST['matchProduct'])) {
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <div>
-<link rel="stylesheet" href="css/style.css?v=<?php echo time();?>">
+<link rel="stylesheet" href="../css/style.css?v=<?php echo time();?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <nav>
