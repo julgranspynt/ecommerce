901bfffab1d83ef3e568d0127395a2a7856de03e
EVELINA <evelinaghazzaoui@hotmail.com>
2022-07-01 22:28:22 +0200

    ok

--- public/searchjs.php
+++ public/searchjs.php
@@ -2,7 +2,6 @@
 <head>
 <title>Search</title>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<style>
     
 $(document).ready(function(e){
     $("#search").keyup(function(){
