308787ae5bd84ed35c4c6c2a5d1068f798a00691
EVELINA <evelinaghazzaoui@hotmail.com>
2022-07-01 22:32:36 +0200

    allt ok

--- public/searchjs.php
+++ public/searchjs.php
@@ -2,7 +2,9 @@
 <head>
 <title>Search</title>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    
+</head>
+<body>
+    <script>
 $(document).ready(function(e){
     $("#search").keyup(function(){
         $("#show_up").show();
