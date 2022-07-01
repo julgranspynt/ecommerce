08873d052197740b7a23b85f4d30628cba9f8531
EVELINA <evelinaghazzaoui@hotmail.com>
2022-07-01 22:29:41 +0200

    Tog bort dubbla stylingen

--- public/searchjs.php
+++ public/searchjs.php
@@ -2,22 +2,6 @@
 <head>
 <title>Search</title>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<style>
-#show_up{
-    width: 200px;
-    height: 200px;
-    border: 1px solid #ddd;
-    display: none;
-}
-#show_up a{
-    border-bottom: 1px solid #ddd;
-    display: block;
-    padding: 5px;
-}
-</style>
-</head>
-<body>
-<script>
     
 $(document).ready(function(e){
     $("#search").keyup(function(){
